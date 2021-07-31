fd035c0392c8145d7d8f07307b34e295d9191727
lel99999 <luan.le@cfpb.gov>
2021-07-30 21:43:51 -0400

    upd network test

--- systools/networktest.py
+++ systools/networktest.py
@@ -1,5 +1,7 @@
-def check_ping():
-    hostname = "www.google.com"
+import os
+
+def check_ping(hostname):
+#   _hostname = "www.google.com"
     response = os.system("ping -c 1 " + hostname)
     # and then check the response...
     if response == 0:
@@ -8,3 +10,24 @@ def check_ping():
         pingstatus = "Network Error"
 
     return pingstatus
+
+def checkhosts(hostfile):
+    i=1
+    try:
+#       f = open("hostlist.txt","r")
+        f = open(hostfile,"r")
+        lines = f.readlines()
+        for line in lines:
+            print(str(i) + ": " + line + "\n")
+            i+=1
+            print("Network Status: " + check_ping(line))
+            
+    except Exception as ex:
+        print(ex)
+
+def main():
+#   print("Network Status: " + check_ping("www.apple.com"))
+    checkhosts("hostlist.txt")
+
+if __name__ == "__main__":
+    main()
